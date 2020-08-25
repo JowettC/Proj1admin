6f805539e17567c69fa8d6f994638c0c4c9f2d8c
JowettC <jowettchng@outlook.com>
2020-08-25 14:30:13 +0800

    add ky to main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Buefy from "buefy";
+import ky from 'ky';
 import "./assets/global-styles.scss";
 
 Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
+const api = ky.create({
+  prefixUrl: "http://3.0.64.28:7000/",
+  throwHttpErrors: false,
+});
+
+Vue.prototype.$http = api;
+
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
