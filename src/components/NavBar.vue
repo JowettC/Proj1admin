3b1595a1678c1b598255bc21f74b453fe98a5d75
Syoongy <mastersyong@gmail.com>
2020-08-28 19:33:49 +0800

    lint

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,51 +1,45 @@
 <template>
-    <b-navbar :spaced="true">
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <picture>
-                    <source
-                    srcset="@/assets/AlphaLogoWhite-small.webp"
-                    type="image/webp"
-                    />
-                    <source
-                    srcset="@/assets/AlphaLogoWhite-small.png"
-                    type="image/png"
-                    />
-                    <img
-                        src="@/assets/AlphaLogoWhite-small.png"
-                        alt="Alpha Logo"
-                    />
-                </picture>
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item href="/workshop">
-                Workshops
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                Downloads
-            </b-navbar-item>
-        </template>
+  <b-navbar :spaced="true">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <picture>
+          <source
+            srcset="@/assets/AlphaLogoWhite-small.webp"
+            type="image/webp"
+          />
+          <source srcset="@/assets/AlphaLogoWhite-small.png" type="image/png" />
+          <img src="@/assets/AlphaLogoWhite-small.png" alt="Alpha Logo" />
+        </picture>
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="/workshop">
+        Workshops
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Downloads
+      </b-navbar-item>
+    </template>
 
-        <template slot="end">
-            <b-navbar-item>
-                Log out
-            </b-navbar-item>
-        </template>
-    </b-navbar>
+    <template slot="end">
+      <b-navbar-item>
+        <b-button>Log out</b-button>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 <script>
 export default {
-    name:'nav-bar'
-}
+  name: "nav-bar",
+};
 </script>
 <style lang="scss" scoped>
 .navbar-item img {
-    max-height:2.75rem !important;
+  max-height: 2.75rem !important;
 }
 @media only screen and (min-width: 1023px) {
-    .navbar-item img {
-        max-height:3.75rem !important;
-    }
+  .navbar-item img {
+    max-height: 3.75rem !important;
+  }
 }
 </style>
