d945c2958c5ea4f2040a5a55ea8342224ca5d45f
yongting1998 <32158771+yongting1998@users.noreply.github.com>
2020-09-03 00:28:44 +0800

    download links

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,19 +1,43 @@
 <template>
   <section id="download-section">
-    <div id="appendingTest">
-      <a>asds</a>
+    <div id="download-link-wrapper">
+      <div class="download-link">
+        <b>All Users</b>
+        <b-button type="is-info" @click="getAllSignUps">Download</b-button>
+      </div>
+      <div class="download-link">
+        <b>All Users that has signed up for workshop</b>
+        <b-button type="is-info" @click="getAllSignUpsWorkshop"
+          >Download</b-button
+        >
+      </div>
     </div>
+    <div id="text-wrapper"></div>
   </section>
 </template>
 <script>
 export default {
-  mounted() {
-    this.getAllSignUps();
-  },
+  // mounted() {
+  //   this.getAllSignUps();
+  // },
   methods: {
     async getAllSignUps() {
       const res = await this.$http
         .get("admin/exportContacts", {
+          headers: { Authorization: `Bearer ${this.$store.state.token}` },
+        })
+        .text();
+      const a = document.createElement("a");
+      a.href = `data:attachment/csv,${encodeURIComponent(res)}`;
+      a.download = "filename.csv";
+      a.target = "_blank";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    },
+    async getAllSignUpsWorkshop() {
+      const res = await this.$http
+        .get("admin/exportAllSignups", {
           headers: { Authorization: `Bearer ${this.$store.state.token}` },
         })
         .text();
@@ -30,9 +54,26 @@ export default {
 </script>
 <style lang="scss" scoped>
 #download-section {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  padding-top: 100px;
   height: 80vh;
+}
+#download-link-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 40%;
+  position: relative;
+  .download-link {
+    margin-left:auto; margin-right:0;
+    display:flex;
+    align-items: center;
+    b{
+      margin:30px;
+    }
+  }
+}
+#text-wrapper {
+  width: 50%;
 }
 </style>
